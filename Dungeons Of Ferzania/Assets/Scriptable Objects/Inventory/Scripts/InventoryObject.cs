73eff0e069ccd9b97e7decf4f67a8af66644ae8f
FarzanZand <farzanzand86@gmail.com>
2021-03-18 21:04:34 +0100

    Added UI art

---
+++ Dungeons Of Ferzania/Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : ScriptableObject
+{
+
+    public List<ItemObject> Container = new List<ItemObject>();
+
+}
